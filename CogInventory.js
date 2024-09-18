d8144044b86e2b9b270c8883249ea75fb0a36c74
DarkNoriss <darknoriss@gmail.com>
2024-09-18 21:35:15 +0200

    Refactor CogInventory.js to update console.log statements for bonusGrid initialization and completion

--- CogInventory.js
+++ CogInventory.js
@@ -331,7 +331,7 @@ class CogInventory {
       expBoost: 0,
       flagBoost: 0,
     };
-    console.log("result initial ", result);
+    console.log("result initial ", result.buildRate);
 
     const board = this.board;
     const bonusGrid = Array(INV_ROWS)
@@ -340,7 +340,7 @@ class CogInventory {
         return Array(INV_COLUMNS)
           .fill(0)
           .map(() => {
-            console.log(result);
+            console.log(result.buildRate);
             return { ...result };
           });
       });
