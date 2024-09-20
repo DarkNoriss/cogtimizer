c741a8d0217a55fa56630b51bf8e832bbeef091e
DarkNoriss <darknoriss@gmail.com>
2024-09-20 05:47:00 +0200

    Refactor CogInventory.js to remove unnecessary console.log statements in position and move methods

--- CogInventory.js
+++ CogInventory.js
@@ -431,7 +431,7 @@ class CogInventory {
       result.expBonus += entry.expBonus || 0;
       result.flaggy += entry.flaggy || 0;
       const pos = entry.position();
-      console.log(pos);
+      console.log("Position: ", pos, "Key: ", entry.key);
       const bonus = bonusGrid[pos.y][pos.x];
       console.log(bonus);
       const b = (bonus.buildRate || 0) / 100;
